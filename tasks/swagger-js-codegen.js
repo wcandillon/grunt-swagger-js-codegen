db008af8939ea43bff1f7a995f81d082e182d901
William Candillon <wcandillon@gmail.com>
2014-06-15 20:23:35 +0200

    Minor refactoring

--- tasks/swagger-js-codegen.js
+++ tasks/swagger-js-codegen.js
@@ -5,8 +5,8 @@ module.exports = function (grunt) {
         var fs = require('fs');
         var CodeGen = require('wagger-js-codegen').CodeGen;
         var options = this.options();
+        var dest = options.dest;
         options.apis.forEach(function(api){
-            var dest = options.dest;
             var swagger = JSON.parse(fs.readFileSync(api.swagger));
             var gen = new CodeGen(api.className, swagger);
             var source = gen.getCode();
