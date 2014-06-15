ec340ecf915d08d03d6eaeab53e78a7849b3ce2f
William Candillon <wcandillon@gmail.com>
2014-06-15 20:18:21 +0200

    First version of the task

--- tasks/swagger-js-codegen.js
+++ tasks/swagger-js-codegen.js
@@ -1,6 +1,16 @@
-'use strict';
-
 module.exports = function (grunt) {
+    'use strict';
+
     grunt.registerMultiTask('swagger-js-codegen', 'Swagger codegen for Javascript', function(){
+        var fs = require('fs');
+        var CodeGen = require('wagger-js-codegen').CodeGen;
+        var options = this.options();
+        options.apis.forEach(function(api){
+            var dest = options.dest;
+            var swagger = JSON.parse(fs.readFileSync(api.swagger));
+            var gen = new CodeGen(api.className, swagger);
+            var source = gen.getCode();
+            fs.writeFileSync(dest + '/' + api.className + '.js', source, 'UTF-8');
+        });
     });
 };
