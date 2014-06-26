556f5e5443d1db37ea9d2da4887673c2af3f7ff5
William Candillon <wcandillon@gmail.com>
2014-06-26 17:20:07 +0200

    Fix support for angularjs

--- tasks/swagger-js-codegen.js
+++ tasks/swagger-js-codegen.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         var dest = options.dest;
         options.apis.forEach(function(api){
             var swagger = JSON.parse(fs.readFileSync(api.swagger));
-            var source = api.angularjs === true ? CodeGen.getNodeCode({ moduleName: api.moduleName, className: api.className, swagger: swagger }) : CodeGen.getNodeCode({ className: api.className, swagger: swagger });
+            var source = api.angularjs === true ? CodeGen.getAngularCode({ moduleName: api.moduleName, className: api.className, swagger: swagger }) : CodeGen.getNodeCode({ className: api.className, swagger: swagger });
             fs.writeFileSync(dest + '/' + api.fileName, source, 'UTF-8');
         });
     });
