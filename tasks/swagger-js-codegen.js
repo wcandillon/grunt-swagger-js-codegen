31d5577a06a7aa2358bd770127cc63029f92a1c7
William Candillon <wcandillon@gmail.com>
2014-10-16 15:10:44 +0200

    Support remote swagger files

--- tasks/swagger-js-codegen.js
+++ tasks/swagger-js-codegen.js
@@ -4,14 +4,56 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('swagger-js-codegen', 'Swagger codegen for Javascript', function(){
         var fs = require('fs');
         var CodeGen = require('swagger-js-codegen').CodeGen;
+        var request = require('request');
+        var Q = require('q');
+        var _ = require('lodash');
+
         var options = this.options();
         var dest = options.dest;
+        var promises = [];
+        var done = this.async();
+
         grunt.file.mkdir(dest);
         options.apis.forEach(function(api){
-            var swagger = JSON.parse(fs.readFileSync(api.swagger));
-            var source = api.angularjs === true ? CodeGen.getAngularCode({ moduleName: api.moduleName, className: api.className, swagger: swagger }) : CodeGen.getNodeCode({ className: api.className, swagger: swagger });
-            grunt.log.writeln('Generated ' + api.fileName + ' from ' + api.swagger);
-            fs.writeFileSync(dest + '/' + api.fileName, source, 'UTF-8');
+            var deferred = Q.defer();
+            if(api.swagger.substring(0, 'http://'.length) === 'http://' || api.swagger.substring(0, 'https://'.length) === 'https://') {
+                request({
+                    uri: api.swagger,
+                    method: 'GET'
+                }, function(error, response, body){
+                    if(error || response.statusCode !== 200) {
+                        deferred.reject('Error while fetching ' + api.swagger + ': ' + (error || body));
+                    } else {
+                        var swagger = JSON.parse(body);
+                        var source = CodeGen.getAngularCode({ moduleName: api.moduleName, className: api.className, swagger: swagger });
+                        grunt.log.writeln('Generated ' + api.moduleName + '-api.js from ' + api.swagger);
+                        fs.writeFileSync(dest + '/' + api.moduleName + '-api.js', source, 'UTF-8');
+                        deferred.resolve();
+                    }
+                });
+            } else {
+                var swagger = JSON.parse(fs.readFileSync(api.swagger, 'UTF-8', function(err, data){
+                    var source = api.angularjs === true ? CodeGen.getAngularCode({ moduleName: api.moduleName, className: api.className, swagger: swagger }) : CodeGen.getNodeCode({ className: api.className, swagger: swagger });
+                    grunt.log.writeln('Generated ' + api.fileName + ' from ' + api.swagger);
+                    fs.writeFileSync(dest + '/' + api.fileName, source, 'UTF-8');
+                }));
+            }
+            promises.push(deferred.promise);
+        });
+        Q.all(promises).then(function(){
+            done();
+        }).catch(function(error){
+            var e;
+            if(_.isObject(error) && error.body) {
+                e = JSON.stringify(error.body, null, 2);
+            } else if(_.isObject(error)) {
+                e = JSON.stringify(error, null, 2);
+            } else if(error instanceof Error || _.isString(error)) {
+                e = error;
+            } else {
+                e = new Error('Unknown error');
+            }
+            grunt.fail.fatal(e);
         });
     });
 };
